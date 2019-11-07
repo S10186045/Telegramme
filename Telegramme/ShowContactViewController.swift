b0ea126d529ba5ff1480c24a6eba103999245278
Victor Jong <s10186045@connect.np.edu.sg>
2019-11-07 11:25:00 +0800

    Delete

--- Telegramme/ShowContactViewController.swift
+++ Telegramme/ShowContactViewController.swift
@@ -41,7 +41,6 @@ class ShowContactViewController : UITableViewController{
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCell.EditingStyle.delete {
-            let contact = appDelegate.contactList[indexPath.row]
             appDelegate.contactList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
         }
